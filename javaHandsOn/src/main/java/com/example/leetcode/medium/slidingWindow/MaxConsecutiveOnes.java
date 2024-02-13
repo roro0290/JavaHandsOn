fd500d480b6e14792ea66740c6bbef6367d4ce70
Roro0290 <rohini_subramanian@outlook.com>
2024-02-13 20:19:56 +0800

    Sliding window - find the max consecutive 1s

---
+++ javaHandsOn/src/main/java/com/example/leetcode/medium/slidingWindow/MaxConsecutiveOnes.java
@@ -0,0 +1,37 @@
+package com.example.leetcode.medium.slidingWindow;
+
+/*
+Given a binary array nums, return the maximum number of consecutive 1's in the array.
+Input: nums = [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s.
+The maximum number of consecutive 1s is 3.
+
+condition =
+ */
+public class MaxConsecutiveOnes {
+
+    public int findMaxConsecutiveOnes(int[] nums) {
+        // define R pointer
+        int R = 0;
+
+        // define max in a row
+        int max = 0, temp = 0;
+        int n = nums.length;
+
+        while (R < n) {
+            if (nums[R] == 1) {
+                temp += 1;
+                max = Math.max(temp, max);
+            } else {
+                temp = 0;
+            }
+            R++;
+        }
+
+        return max;
+
+    }
+
+
+}
