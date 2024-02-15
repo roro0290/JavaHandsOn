3fa165482aca2ad76b3f15b72d85b1df72da1dcf
Roro0290 <rohini_subramanian@outlook.com>
2024-02-15 21:08:59 +0800

    Leetcode: 12 Integer to Roman with Hashmap

--- javaHandsOn/src/main/java/com/example/leetcode/medium/IntegerToRoman.java
+++ javaHandsOn/src/main/java/com/example/leetcode/medium/IntegerToRoman.java
@@ -9,6 +9,9 @@ Convert the number from Integer back to Roman
  */
 public class IntegerToRoman {
 
+    /*
+    greedy style implementation where we remove the largest possible value from it
+     */
     public String intToRoman(int num) {
 
         StringBuilder result = new StringBuilder();
@@ -36,10 +39,10 @@ public class IntegerToRoman {
     https://leetcode.com/problems/integer-to-roman/solutions/2962674/easiest-o-1-faang-method-ever
      */
     String intToRoman2(int num) {
-        String ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
-        String tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
-        String hrns[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
-        String ths[]={"","M","MM","MMM"};
+        String[] ones = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        String[] tens = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        String[] hrns = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        String[] ths ={"","M","MM","MMM"};
 
         return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
     }
